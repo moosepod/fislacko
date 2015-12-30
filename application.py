125dbc6d9a43160379b6d1b4e94836f6f7be0228
Matt Christensen <mchristensen@moosepod.com>
2015-12-30 14:05:19 -0500

    Stubs for all commands

--- application.py
+++ application.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 api = Api(app)
 
 COMMAND_MAPPINGS = {'reset_game': commands.reset_game,
-                    'register': commands.register}
+                    'register': commands.register,
+                    'status': commands.status,
+                    'claim': commands.claim,
+                    'give': commands.give,
+                    'roll_pool': commands.roll_pool,
+                    'roll': commands.roll,
+                    'spend': commands.spend}
                     
 
 @app.route('/game/<game_id>/',methods=['POST','GET'])
@@ -16,14 +22,23 @@ def router(game_id):
     userid = request.form.get('user_id')
     username = request.form.get('user_name')
     command = None
-    params = None
+    params = []
     if data:
         command = COMMAND_MAPPINGS.get(data[0].lower())
         if len(data) > 1:
             params = data[1:]
     if command:
         return jsonify(command(params,userid,username).to_json())
-    return u'Error! Unknown command'
+    return u"""Usage: /slack command, where commands are:
+reset_game: reset the game
+pool: roll the dice for the pool
+register [name]: register your player name with the game
+status: request that the current game status be output to the channel
+claim [color number]: claim a die from the pool
+give [color number user]: give a die to another player
+roll: roll all your dice and give the aggregate score
+spend: spend one your dice (so you no longer have it)"""
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='104.236.212.18')
--- commands.py
+++ commands.py
@@ -20,7 +20,40 @@ def reset_game(params,user_id,user_name):
 
 def register(params,user_id,user_name):
     """ Register the logged in user as a given character """
-    if len(params):
+    # Take in name
+    # Store name
+    if not len(params):
         return SlackResponse('Please provide the name you will go by.')
     name = ' '.join(params)
     return SlackResponse('%s is now registered as %s' % (user_name,name),True)
+
+def status(params,user_id,user_name):
+    """ Send game status to channel """
+    # NO parameters
+    return SlackResponse("Status: test",True)
+
+def claim(params,user_id,user_name):
+    """ Claim a specific die """
+    # Take color + number
+    # Verify die
+    # take die
+    # Print status
+    return SlackResponse("%s claimed die" % (user_name),True)
+
+def give(params,user_id,user_name):
+    """ Give one of your dice to someone else """
+    return SlackResponse("%s gave dice" % (user_name),True)
+
+def roll(params,user_id,user_name):
+    """ Roll a user's dice and show the sum """
+    return SlackRespones("%s rolled: " % user_name,True)
+
+def roll_pool(params,user_id,user_name):
+    """ Reroll the central dice pool """
+    return SlackResponse("Pool recreated.",True)
+
+def spend(params,user_id,user_name):
+    """ Let a user spend one of their dice """
+    # Take color and number
+    # Verify
+    return SlackResponse("Spent")
